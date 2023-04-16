6ac8f224b63d5b2f19fa92eca6ae9203025c8db5
LVik <tips_liborv@hotmail.com>
2023-04-16 09:09:39 -0400

    first change

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           With nodemon, edit <code>src/App.js</code> and save will reload.
         </p>
+        <p>With Amplify, committing the main branch will redeploy in aws.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
